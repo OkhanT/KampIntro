37b1486289f29a8420b701aacf1ebb9a0290aa41
OkhanT <m.okhan.tanriverdi@hotmail.com>
2021-11-27 02:36:43 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety
+            //Do not repaet yourself - Kendini tekrarlama
+            //Değer tutucu, alias (sahte ad)
+
+            //veriType
+            string kategoriEtiketi = "Kategoriler";
+            int ogranciSayisi = 3200;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = false;
+
+            double dolarDun = 7.55;
+            double dolarBugun = 7.55;
+
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış Butonu");
+            }
+            else if(dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış Butonu");
+            }
+            else
+            {
+                Console.WriteLine("değişmedi butonu");
+            }
+
+            //şartBlokları
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+
+            Console.WriteLine(kategoriEtiketi);
+        }
+    }
+}
