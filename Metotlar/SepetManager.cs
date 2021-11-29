3a6b43e4d659c4b8bb8d8fc8bafe7c72e7a5b53d
OkhanT <m.okhan.tanriverdi@hotmail.com>
2021-11-29 21:19:43 +0300

    20 Ocak 2021 Ders Sonu

---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+
+        //naming convention - isimlendirme kuralı
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("Tebrikler, Sepete Eklendi! : " + urun.Adi);
+
+        }
+        public void Ekle2(string urunadi, string aciklama, double fiyat, int stokAdedi)
+        {
+            Console.WriteLine("Tebrikler. Sepete eklendi : " + urunadi);
+        }
+    }
+}
