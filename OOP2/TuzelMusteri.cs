0c4c89db66e570106ed5d01ae108fd69831c7a71
OkhanT <m.okhan.tanriverdi@hotmail.com>
2021-11-30 23:50:09 +0300

    23 Ocak Ders Sonu

---
+++ OOP2/TuzelMusteri.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2
+{
+    //Coorporate -- TuzelMusteri
+    //miras - inheratance
+    class TuzelMusteri:Musteri          //inheratance
+    {
+        public string SirketAdi { get; set; }
+        public string VergiNo { get; set; }
+    }
+}
